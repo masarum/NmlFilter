263a0c50cf4792c3a406f8e6901e3643d039bc19
Morikawa, Masaru <voxlet.msr@gmail.com>
2011-03-09 03:46:24 +0900

    correctly order playlist

--- src/NmlFilter/core.clj
+++ src/NmlFilter/core.clj
@@ -2,30 +2,56 @@
   (:gen-class)
   (:require [clojure.xml :as xml])
   (:require [clojure.zip :as zip])
-  (:require [clojure.contrib.zip-filter.xml :as zf])
+  (:require [clojure.contrib.zip-filter.xml :as zfx])
+  (:require [clojure.contrib.zip-filter :as zf])
   (:require [clojure.contrib.string :as str]))
 
 
 (defn titles [zipper]
-  (zf/xml-> zipper :COLLECTION :ENTRY (zf/attr :TITLE)))
+  (zfx/xml-> zipper :COLLECTION :ENTRY (zfx/attr :TITLE)))
 
 (defn artists [zipper]
-  (zf/xml-> zipper :COLLECTION :ENTRY (zf/attr :ARTIST)))
+  (zfx/xml-> zipper :COLLECTION :ENTRY (zfx/attr :ARTIST)))
+
+(defn primary-keys [zipper]
+  (for [[volume dir file]
+	(map vector
+	(zfx/xml-> zipper :COLLECTION :ENTRY :LOCATION (zfx/attr :VOLUME))
+	(zfx/xml-> zipper :COLLECTION :ENTRY :LOCATION (zfx/attr :DIR))
+	(zfx/xml-> zipper :COLLECTION :ENTRY :LOCATION (zfx/attr :FILE)))]
+    (str volume dir file)))
+
+(defn entries [zipper]
+  (zipmap
+   (primary-keys zipper)
+   (map vector (artists zipper) (titles zipper))))
+
+(defn playlist [zipper]
+  (zfx/xml-> zipper
+	    :PLAYLISTS
+	    :NODE
+	    :SUBNODES
+	    :NODE
+	    :PLAYLIST
+	    :ENTRY
+	    :PRIMARYKEY
+	    (zfx/attr :KEY)))
+
+(defn ordered-entries [zipper]
+  (for [key (playlist zipper)]
+    (find (entries zipper) key)))
 
 (defn normalize [title]
   (let [[name remix] (str/split #" - " title)]
     (if (or (not remix)
-	    (= (compare remix "Original Mix") 0))
+	    (= remix "Original Mix"))
       name
       (format "%s (%s)" name remix))))
 
 (defn tracklist [zipper]
-  (for [[index title artist]
-	(map vector
-	     (range)
-	     (titles zipper)
-	     (artists zipper))]
-    (format "%d. %s - %s" (+ index 1) artist (normalize title))))
+  (for [[index entry] (map vector (range) (vals (ordered-entries zipper)))]
+    (let [[artist title] entry]
+      (format "%d. %s - %s" (+ index 1) artist (normalize title)))))
 
 (defn pretty-print [nml]
   (doseq [track (tracklist (zip/xml-zip (xml/parse nml)))]
